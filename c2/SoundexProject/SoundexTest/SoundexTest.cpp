bded45b6e735daef7de61166a6b683903b8a9085
GD <stefan.power@gmail.com>
2018-05-25 23:35:30 +0200

    gmock_main included in CMakeList and in system, Soundex return A

--- c2/SoundexProject/SoundexTest/SoundexTest.cpp
+++ c2/SoundexProject/SoundexTest/SoundexTest.cpp
@@ -1,10 +1,24 @@
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include <string>
+
 
 class Soundex 
 {
+public:
+	std::string encode(const std::string &word)const
+	{
+		return "A";
+	}
+
 };
 
 TEST (SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
 {
 	Soundex soundex;
+
+	std::string encoded = soundex.encode("A");
+	
+	ASSERT_THAT(encoded, testing::Eq("A"));
+
 }
